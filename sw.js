4abff5a80839ee1fd5bac787e5ffe3d67b66fc4e
karlamendozad <71843945+karlamendozad@users.noreply.github.com>
2021-03-09 17:10:05 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/pwa-examples/a2hs/',
+      '/pwa-examples/a2hs/index.html',
+      '/pwa-examples/a2hs/index.js',
+      '/pwa-examples/a2hs/style.css',
+      '/pwa-examples/a2hs/images/diablo-squad.jpg',
+      '/pwa-examples/a2hs/images/diablosquad.jpg'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
