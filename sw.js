183d1844c1f31438770d6051e497398618d22e71
karlamendozad <71843945+karlamendozad@users.noreply.github.com>
2021-03-09 17:29:00 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,10 +5,10 @@ self.addEventListener('install', (e) => {
       '/pwa-examples/a2hs/index.html',
       '/pwa-examples/a2hs/index.js',
       '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/imagen/img1.jpg',
-      '/pwa-examples/a2hs/imagen/img2.jpg',
-      '/pwa-examples/a2hs/imagen/img3.jpg',
-      '/pwa-examples/a2hs/imagen/img4.jpg'
+      '/pwa-examples/a2hs/images/img1.jpg',
+      '/pwa-examples/a2hs/images/img2.jpg',
+      '/pwa-examples/a2hs/images/img3.jpg',
+      '/pwa-examples/a2hs/images/img4.jpg'
     ])),
   );
 });
